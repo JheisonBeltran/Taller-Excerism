5ef1bab7e95af843d44848eb9cafe56697d7c230
Jheison Beltran <jeisonbeltrantorres@gmail.com>
2021-04-05 20:16:24 -0500

    Taller Exercism ejercicio2

--- taller2.py
+++ taller2.py
@@ -6,7 +6,11 @@ Created on Mon Apr  5 17:49:01 2021
 """
 
 # TALLER DE EXCERISM - CORTE 2
-# EJERCICIO 1 - TALLER DE EXCERISM
+
+# EJERCICIO 1 - Two Fer
+# Dado un nombre, devuelva una cadena con el mensaje: One for X, one for me.
+# Donde X es el nombre dado.
+# ‎Si falta el nombre, devuelva el st‎ring: One for you, one for me.
 
 import unittest
 
@@ -18,11 +22,13 @@ class TwoFerTest(unittest.TestCase):
         self.assertEqual(two_fer(), "One for you, one for me.")
 
     def test_a_name_given(self):
-        self.assertEqual(two_fer("Alice"), "One for Alice, one for me.")
+        self.assertEqual(two_fer("Leonela"), "One for Leonela, one for me.")
 
     def test_another_name_given(self):
-        self.assertEqual(two_fer("Bob"), "One for Bob, one for me.")
+        self.assertEqual(two_fer("Jorge"), "One for Jorge, one for me.")
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
